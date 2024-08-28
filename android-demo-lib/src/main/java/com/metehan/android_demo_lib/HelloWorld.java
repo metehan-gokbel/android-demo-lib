a6ebe1ada69462b28d4e75c76f6c096ad818f293
MetehanGokbel <metehan.gokbel@enqura.com>
2024-08-28 18:27:58 +0300

    init commit

---
+++ android-demo-lib/src/main/java/com/metehan/android_demo_lib/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.metehan.android_demo_lib;
+
+import android.util.Log;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Main Method
+    }
+
+    public static void printLog(String tag, String message) {
+        Log.d(tag, message);
+    }
+}
